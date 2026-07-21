ae66ca7375cdff7e8bffa0c49223977f6613fb34
Alliance-Sky <allianceskygit@gmail.com>
2026-07-21 11:10:52 +0530

    Use copyright symbol instead of text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ export default function Footer() {
         </div>
 
         <p className="footer-copyright font-mono">
-          Copyright {new Date().getFullYear()} Musaddik Temkar. Built with React & Vite.
+          © {new Date().getFullYear()} Musaddik Temkar. Built with React & Vite.
         </p>
       </div>
     </footer>
